2437e56ec1e89057eb17a53d952d85ab7c071429
Tomer du Sautoy <tomerdusautoy@gmail.com>
2019-06-03 17:17:59 -0500

    first commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,54 @@
-import React from 'react';
 import './Header.css';
+
+import React from 'react';
+
+
+class HeaderContainer extends React.Component {
+  render() {
+    return (
+      <div className="HeaderContainer">
+        <HeaderTitle />
+        <ImageThumbnail />
+      </div>
+    )
+  }
+}
+
+class ImageThumbnail extends React.Component {
+  render() {
+    return (
+      <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="react"></img>
+    )
+  }
+}
+
+class HeaderTitle extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleString()
+    };
+  }
+
+  render() {
+    return (
+      <div className="header-title">
+        <h1>Lambda School</h1>
+        <h2>@LambdaSchool</h2>
+        <p>{this.state.date}</p>
+      </div>
+    )
+  }
+}
+
+class HeaderContent extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p className="header-content">Hodor Hodor Hodor HodoHodor Hodor Hodor Hodor Hodor Hodorr Hodor Hodor Hodor Hodor Hodor Hodor Hodor Hodor </p>
+      </div>
+    )
+  }
+}
